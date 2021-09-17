7ec22c76e2f2c474d0df3fd3b747c0931fd74adf
Josiel da Costa Carvalho <josiel.jcc@hotmail.com>
2021-09-16 22:57:04 -0300

    end config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,38 @@
-const patch = require('path')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  entry: patch.resolve(__dirname, 'src', 'index.jsx'),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
-    patch: patch.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
   }
 }
